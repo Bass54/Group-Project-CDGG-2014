c649b27fa4bd37b51a7bbbab950cb3f15efd753b
LunaEclipsa <Kackah123@icloud.com>
2025-03-25 10:03:40 -0400

    player follwer stuff

--- Assets/Scenes/Player Follower Scene files/Scene scripts/PlayerFollower.cs
+++ Assets/Scenes/Player Follower Scene files/Scene scripts/PlayerFollower.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class FollowPlayerNavMesh : MonoBehaviour
+public class FollowPlayerNavMesh1 : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Transform followTarget;
