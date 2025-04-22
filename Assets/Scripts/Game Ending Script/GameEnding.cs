f53668694e488001b779188056ec71b31310cc83
Tbeene <tahjirb@gmail.com>
2025-04-22 13:14:27 -0400

    Added some stuff

--- Assets/Scripts/Game Ending Script/GameEnding.cs
+++ Assets/Scripts/Game Ending Script/GameEnding.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using Controller;
+using TMPro;
 
 public class GameEnding : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public CanvasGroup escapeBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    float m_Timer;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PinguinMover.gameLost || PinguinMover.gameWon)
+        {
+            endGame();
+        }
+    }
+    void endGame()
+    {
+        m_Timer += Time.deltaTime;
+        if (PinguinMover.gameWon)
+        {
+            escapeBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        }
+        if (PinguinMover.gameLost)
+        {
+            caughtBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        }
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
     }
 }
